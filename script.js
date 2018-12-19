3d4731dcb5ef4d0e665df585f31646c4b3902c47
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-12-19 16:06:13 +0500

    added basic style

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let wind = document.getElementById("wind-speed");
 let change = document.getElementById("change");
 let search = document.getElementById("search").value;
 
+function reload() {
+   location.reload();
+}
+
 function Submit() {
    fetch('http://api.openweathermap.org/data/2.5/weather?q='+ search + '&APPID=836ae263c4d8ea9e2a2657878ceaea61', {mode: 'cors'})
    .then(function(response) {
@@ -12,13 +16,12 @@ function Submit() {
    })
    .then(function(response) {
      let weather = response.main.temp
-     temperature.innerHTML = weather - 273.15 + '°C';
+     temperature.innerHTML = Math.floor(weather - 273.15) + '°C';
      city.innerHTML = response.name
      description.innerHTML = response.weather[0].description
      wind.innerHTML = response.wind.speed
      change.addEventListener('click', ()  => {
-        temperature.innerHTML = (weather - 273.15) * 9/5 + 32 + '°F'; 
+        temperature.innerHTML = Math.floor(weather - 273.15) * 9/5 + 32 + '°F'; 
      })
-     console.log("yay!!")
   })
 };
