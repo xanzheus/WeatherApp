ffdc3b122ac70ffc479f4ed2b74323038bd7011b
AREEBAISHTIAQ <areeba.aic@gmail.com>
2018-12-20 15:51:09 +0500

    added background videos

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let description = document.getElementById("description");
 let wind = document.getElementById("wind-speed");
 let change = document.getElementById("change");
 let search = document.getElementById("search").value;
+let icon = document.getElementById("icon");
+let main = document.getElementById("main");
+let humidity = document.getElementById("humidity");
 
 function reload() {
    location.reload();
@@ -19,9 +22,40 @@ function Submit() {
      temperature.innerHTML = Math.floor(weather - 273.15) + '°C';
      city.innerHTML = response.name
      description.innerHTML = response.weather[0].description
+     main.innerHTML = response.weather[0].main
+     humidity.innerHTML = response.main.humidity + "%"
+     icon.src = ("https://openweathermap.org/img/w/") + response.weather[0].icon + '.png'
      wind.innerHTML = response.wind.speed
      change.addEventListener('click', ()  => {
+        c = Math.floor(weather - 273.15) + '°C';
+        f = Math.floor(weather - 273.15) * 9/5 + 32 + '°F';
+        if (temperature.innerHTML == c) {
         temperature.innerHTML = Math.floor(weather - 273.15) * 9/5 + 32 + '°F'; 
+        } else if (temperature.innerHTML == f) {
+           temperature.innerHTML = Math.floor(weather - 273.15) + '°C';
+        }
      })
+     if (response.weather[0].description == "clear sky") {
+        let v1 = document.createElement('VIDEO');
+        v1.setAttribute("src", "Clouds_Timelapse_66___30s___4k_res.mp4");
+        v1.setAttribute("class", "v1")
+        document.body.appendChild(v1)
+        v1.autoplay = true;
+        v1.load();
+     } else if (response.weather[0].description == "few clouds") {
+         let v1 = document.createElement('VIDEO');
+         v1.setAttribute("src", "Clouds_Timelapse_66___30s___4k_res.mp4");
+         v1.setAttribute("class", "v1")
+         document.body.appendChild(v1)
+         v1.autoplay = true;
+         v1.load();
+     } else if (response.weather[0].description == "few clouds") {
+         let v1 = document.createElement('VIDEO');
+         v1.setAttribute("src", "Clouds_Timelapse_66___30s___4k_res.mp4");
+         v1.setAttribute("class", "v1")
+         document.body.appendChild(v1)
+         v1.autoplay = true;
+         v1.load();
+  }
   })
 };
